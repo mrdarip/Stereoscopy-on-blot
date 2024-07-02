90bf38894eefb9d7886322c8e28c7c2ebf782863
mrdarip <darioelas2005@gmail.com>
2024-07-02 15:33:20 +0200

    implemented vector2 class and some methods for it

--- stereoscope.js
+++ stereoscope.js
@@ -13,3 +13,31 @@ const finalLines = [];
 
 // draw it
 drawLines(finalLines);
+
+
+
+
+
+
+
+class Vec2 {
+  constructor(x, y) {
+      this.x = x;
+      this.y = y;
+  }
+
+  static lerp(a, b, t) {
+      return new Vec2(
+          a.x + (b.x - a.x) * t,
+          a.y + (b.y - a.y) * t
+      );
+  }
+
+  plus(dx, dy) {
+      return new Vec2(this.x + dx, this.y + dy);
+  }
+
+  minus(dx, dy) {
+      return new Vec2(this.x - dx, this.y - dy);
+  }
+}
