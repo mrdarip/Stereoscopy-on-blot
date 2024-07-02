d428fe904f90b73c71ed7866eb4e4d72e170a8d5
mrdarip <darioelas2005@gmail.com>
2024-07-02 19:52:16 +0200

    implemeted the line-line intersection method

--- stereoscope.js
+++ stereoscope.js
@@ -33,6 +33,25 @@ class Vec2 {
       );
   }
 
+
+    // line-line intersection
+    static lli(l1p1, l1p2, l2p1, l2p2) {
+        let a1 = l1p2.y - l1p1.y;
+        let b1 = l1p1.x - l1p2.x;
+        let c1 = a1 * l1p1.x + b1 * l1p1.y;
+        let a2 = l2p2.y - l2p1.y;
+        let b2 = l2p1.x - l2p2.x;
+        let c2 = a2 * l2p1.x + b2 * l2p1.y;
+        let det = a1 * b2 - a2 * b1;
+        if (det === 0) {
+            return null; // the lines are parallel
+        } else {
+            let x = (b2 * c1 - b1 * c2) / det;
+            let y = (a1 * c2 - a2 * c1) / det;
+            return new Vec2(x, y);
+        }
+    }
+  
   plus(dx, dy) {
       return new Vec2(this.x + dx, this.y + dy);
   }
